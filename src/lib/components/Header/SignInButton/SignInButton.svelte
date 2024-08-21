176ebb4a889c591f9e1fb89f6e65a2a28af16235
Dima <dimalebedev6902@gmail.com>
2024-08-21 13:18:16 +0300

    feat: implemented authentication with API, added typescript, WalletService, logout

--- src/lib/components/Header/SignInButton/SignInButton.svelte
+++ src/lib/components/Header/SignInButton/SignInButton.svelte
@@ -1,13 +1,18 @@
 <script>
   import mobileSigninIcon from "../../../../assets/icons/sign-in.png"
   import { toggleModalState } from "../../../stores/modalsStore"
-  import { userState } from "../../../stores/userStore"
+  import { setUserState, userState } from "../../../stores/userStore"
+
+  const onButtonClick = () => {
+    if ($userState.user) {
+      setUserState(null)
+    } else {
+      toggleModalState("isAuthModalOpen", true)
+    }
+  }
 </script>
 
-<button
-  class="header-button"
-  on:click={() => toggleModalState("isAuthModalOpen", true)}
->
+<button class="header-button" on:click={onButtonClick}>
   {$userState.user ? $userState.user.username : "Sign in"}
   <img src={mobileSigninIcon} alt="sign-in-button" class="header-button-icon" />
 </button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,74 @@
-<script>
+<script lang="ts">
   import "../styles/app.css"
   import Header from "../lib/components/Header/Header.svelte"
   import Footer from "../lib/components/Footer/Footer.svelte"
+  import { onDestroy, onMount } from "svelte"
+  import { gameOrigin, wallet } from "../utils/global"
+  import { setUserState } from "../lib/stores/userStore"
+  import { browser } from "$app/environment"
 
- 
+  // Refresh user info on mount
+
+  onMount(() => {
+    const token: string | null = localStorage.getItem("token")
+
+    const fetchMyUser = async () => {
+      const userInfo = await wallet.member.me(token!)
+      if (userInfo.username) {
+        setUserState(userInfo)
+      }
+    }
+
+    if (token) {
+      fetchMyUser()
+    }
+  })
+
+  /*
+   * Token transfer implementation
+   *
+   * To open the game in new tabbed window use window.open(url, '_blank');
+   * All game clients have been registered to trust https://ryzer-main.vercel.app origin (and http://site1.local but that's only for
+   * local development). After game client window is loaded, it will send a message { type: "request_token" } to it's opener window
+   * (this window). In onMessage function, you check the origin of the message to make sure it's from the game client,
+   * you can set the trusted game clients origin in a variable somewhere (I used gameOrigin here). If the message is from the game client,
+   * then the type of message is checked and if it's "request_token" then this window will send a message back to the game client
+   * with the type set to 'token' and token set to the user token. It's very important to specify the "targetOrigin" when sending the message
+   * back to the game client to make sure that ONLY game origin can receive the message.
+   */
+
+  function onMessage(event: MessageEvent<any>) {
+    const userToken = localStorage.getItem("token")
+
+    if (!userToken) return
+
+    if (event.origin != gameOrigin) {
+      // very important! this is to prevent any untrusted source from sending messages to this window
+      return
+    }
+
+    if (event.data.type == "request_token") {
+      event.source?.postMessage(
+        {
+          type: "token",
+          token: userToken
+        },
+        { targetOrigin: gameOrigin }
+      ) // <- also very important! this is to make sure that only game client window can receive the message
+    }
+  }
+
+  onMount(() => {
+    if (browser) {
+      window.addEventListener("message", onMessage)
+    }
+  })
+
+  onDestroy(() => {
+    if (browser) {
+      window.removeEventListener("message", onMessage)
+    }
+  })
 </script>
 
 <Header />
