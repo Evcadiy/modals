e39abb36c43597fa2244f6902a61afe91b47516c
Evcadiy <nikusha1305@gmail.com>
2024-10-05 11:58:08 +0300

    some corrections in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,43 +5,31 @@ export default {
     extend: {
       // Font size
       fontSize: {
-        xs: ["0.75rem", "1rem"], // text-xs: Font Size: 0.75rem | Line Height: 1rem
-        sm: ["0.875rem", "1.25rem"], // text-sm: Font Size: 0.875rem | Line Height: 1.25rem
-        lg: ["1.125rem", "1.75rem"], // text-lg: Font Size: 1.125rem | Line Height: 1.75rem
-        xl: ["1.25rem", "1.75rem"], // text-xl: Font Size: 1.25rem | Line Height: 1.75rem
-        "2xl": ["1.5rem", "2rem"], // text-2xl: Font Size: 1.5rem | Line Height: 2rem
-        "6xl": ["3.75rem", "3.75rem"], // text-6xl: Font Size: 3.75rem | Line Height: 3.75rem
-        "7xl": ["4.5rem", "4.5rem"], // text-7xl: Font Size: 4.5rem | Line Height: 4.5rem
-        "8xl": ["6rem", "6rem"] // text-8xl: Font Size: 6rem | Line Height: 6rem
+        "text-xs": ["0.75rem", { lineHeight: "1rem", fontWeight: "700" }],
+        "text-sm": ["0.875rem", { lineHeight: "1.25rem", fontWeight: "600" }],
+        "text-sm_eb": [
+          "0.875rem",
+          { lineHeight: "1.25rem", fontWeight: "800" }
+        ],
+        "text-lg": ["1.125rem", { lineHeight: "1.75rem", fontWeight: "800" }],
+        "text-lg_sb": [
+          "1.125rem",
+          { lineHeight: "1.75rem", fontWeight: "600" }
+        ],
+        "+text-xl": ["1.25rem", { lineHeight: "1.75rem", fontWeight: "800" }],
+        "+sb_text-xl": [
+          "1.25rem",
+          { lineHeight: "1.75rem", fontWeight: "600" }
+        ],
+        "+text-2xl": ["1.5rem", { lineHeight: "2rem", fontWeight: "600" }],
+        "button-2xl": ["1.5rem", { lineHeight: "2rem", fontWeight: "800" }],
+        "+text-6xl": ["3.75rem", { lineHeight: "3.75rem", fontWeight: "800" }],
+        "+text-7xl": ["4.5rem", { lineHeight: "4.5rem", fontWeight: "800" }],
+        "+text-8xl": ["6rem", { lineHeight: "6rem", fontWeight: "800" }]
       },
       // Font families
       fontFamily: {
-        sans: [
-          "Open Sans",
-          "Inter",
-          "ui-sans-serif",
-          "system-ui",
-          "-apple-system",
-          "BlinkMacSystemFont",
-          "Segoe UI",
-          "Roboto",
-          "Helvetica Neue",
-          "Arial",
-          "Noto Sans",
-          "sans-serif",
-          "Apple Color Emoji",
-          "Segoe UI Emoji"
-        ]
-      },
-      // Colors
-      colors: {},
-      // Shadow boxes
-      boxShadow: {
-        "inner-light": "inset 0 -3px 0 #3ee997"
-      },
-      // Containers max widths
-      maxWidth: {
-        container: "var(--max-container-width)"
+        sans: ["Open Sans", "Inter"]
       },
       // Breakpoints for media queries
       screens: {
