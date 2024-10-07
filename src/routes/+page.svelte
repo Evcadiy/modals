31feb1b89f39e042359e928277f032485b510a1b
Evcadiy <nikusha1305@gmail.com>
2024-10-07 14:53:16 +0300

    feat: create buttons for modal selection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,47 @@
 <script>
+  import FirstModal from "$lib/components/Modals/FirstModal.svelte"
   import SecondModal from "$lib/components/Modals/SecondModal.svelte"
+
+  let activeModal = null
+
+  const showModal = (modal) => {
+    activeModal = modal
+  }
 </script>
 
-<section class="flex items-center justify-center h-screen bg-opacity-50">
-  <SecondModal />
+<section
+  class="flex flex-col items-center justify-center h-screen bg-opacity-50"
+>
+  <div class="my-4">
+    <button
+      on:click={() => showModal("first")}
+      class="bg-blue-500 text-white py-2 px-4 rounded m-2"
+    >
+      First Modal
+    </button>
+    <button
+      on:click={() => showModal("second")}
+      class="bg-blue-500 text-white py-2 px-4 rounded m-2"
+    >
+      Second Modal
+    </button>
+    <button
+      on:click={() => showModal("third")}
+      class="bg-blue-500 text-white py-2 px-4 rounded m-2"
+    >
+      Third Modal
+    </button>
+    <button
+      on:click={() => showModal("fourth")}
+      class="bg-blue-500 text-white py-2 px-4 rounded m-2"
+    >
+      Fourth Modal
+    </button>
+  </div>
+
+  {#if activeModal === "first"}
+    <FirstModal />
+  {:else if activeModal === "second"}
+    <SecondModal />
+  {/if}
 </section>
